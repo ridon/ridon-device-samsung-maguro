547c453e2fefcfe21a39fd60b5d9777ce0a17abb
Mohammad Anwari <mdamt@mdamt.net>
2014-11-29 15:34:28 +0700

    Initial Ridon 5.0 commit

--- cm.mk
+++ cm.mk
@@ -1,17 +1,15 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/ridon/config/gsm.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := GN-GSM
 
 # Boot animation
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/ridon/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/ridon/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
@@ -19,9 +17,9 @@ $(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := maguro
 PRODUCT_NAME := ridon_maguro
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
