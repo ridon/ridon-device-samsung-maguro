462fb6e5dd1dde747ef03c3e25f8732ce07a2420
Mohammad Anwari <mdamt@mdamt.net>
2014-11-29 16:09:17 +0700

    Inherit ridon mk

--- cm.mk
+++ cm.mk
@@ -6,10 +6,10 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/ridon/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ridon/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/aosp_maguro.mk)
