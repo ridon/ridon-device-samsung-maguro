cb8615cb817209bf60a7998a1e9ee8ab0dfd562f
Mohammad Anwari <mdamt@mdamt.net>
2015-01-06 15:28:13 +0700

    Remove unused files

--- device.mk
+++ device.mk
@@ -24,13 +24,5 @@ DEVICE_PACKAGE_OVERLAYS := device/samsung/maguro/overlay
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-# Audio
-PRODUCT_PACKAGES += \
-	libaudio-ril-interface
-
-# RIL
-PRODUCT_PACKAGES += \
-	libsamsung-ril
-
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/maguro/device-vendor.mk)
