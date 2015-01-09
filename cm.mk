c13d45f78626b743a99e97949edbe54185641324
Mohammad Anwari <mdamt@mdamt.net>
2015-01-09 07:48:08 +0700

    Rename

--- cm.mk
+++ cm.mk
@@ -19,7 +19,7 @@ PRODUCT_DEVICE := maguro
 PRODUCT_NAME := ridon_maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.3 JWR66Y 776638 release-keys"
